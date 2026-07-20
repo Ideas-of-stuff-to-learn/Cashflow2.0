577145e1ff02933eb3a4039b4d6c4b380d9c8bdf
Armaan Khaitan <khaitanarmaan@gmail.com>
2026-07-21 00:35:57 +0100

    updated tasks

--- App/UI/App.js
+++ App/UI/App.js
@@ -1,21 +1,22 @@
 /*
 
 Tasks to do:
-  Road map accurate to deployed to play store and app store
-    Actual process
-    And costs to maintain like backend free tier, google Gemini Api payment plan, google play store price
-    Research what is needed to pass the review
-  Make into windows app wrapped
-  Deploying to Microsoft store
-    process and price
-  Make into website
-  Manually categorise pop resolve feature should say top right the number of manually categorise things left to manually categorise DONE
-  Homepage should say processing will take this long [approx.] you can close the app and come back in a bit
-  Backfill should only work for adjacent years. If there is a gap there is the previous month with blank bars or no bars
-  Organise code into more structured format
-  Test higher cache and batch size to speed up potentially
-  Investigate fuzzy matching bottleneck
-  Restructure contents screen DONE
+
+Pause everything and make current working one into a website
+    This is not insignificant
+        For client's understand I have written in react native so writing the website in r react means I can reuse some logic, but I will have to rewrite every single file that outputs a visible thing toa different syntax
+        I must find a strong equivalent of expo secure store on web
+        I must account for CORS and potentially need to add middleware to trick IoS browsers
+        
+Road map accurate to deployed to play store and app store
+Actual process
+And costs to maintain like backend free tier, google Gemini Api payment plan, google play store price
+Research what is needed to pass the review
+Make into windows app wrapped
+Deploying to Microsoft store, process and price
+Homepage should say processing will take this long [approx.] add timer done the whle what batch am I on you can close the app and come back in a bit
+Backfill should only work for adjacent years. If there is a gap there is the previous month with blank bars or no bars
+Organise code into more structured format: login screen, signup screen
 */
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
