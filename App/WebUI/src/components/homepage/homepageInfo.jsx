e712517ffceb54208c12c8deba031f0afebe836d
Armaan Khaitan <khaitanarmaan@gmail.com>
2026-08-05 10:48:51 +0100

    fv

--- App/WebUI/src/components/homepage/homepageInfo.jsx
+++ App/WebUI/src/components/homepage/homepageInfo.jsx
@@ -3,9 +3,7 @@ import '../../styles/homePage.css';
 export default function HomepageInfo({ dateRangeInfo, uploadCount }) {
     return (
         <>
-            <h1 className="title">Transaction Categorizer</h1>
-            <p className="important-msg">This app does NOT connect to banks, it is strictly CSV upload only</p>
-            <p className="subtitle">Upload a CSV to get started</p>
+            <h1 className="title">Spending Pattern Visualisation Tool</h1>
             <p className="range-text">
                 Number of past files uploaded : {uploadCount}
             </p>
